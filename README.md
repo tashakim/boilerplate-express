52af92c850fb3ddb27be3c82bfd186d7e99562ee
Tasha Kim <jae_young_kim@brown.edu>
2020-09-14 20:36:11 +0900

    Extend express boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Backend Challenges Boilerplate - Basic Express
+# Backend Boilerplate - Basic Express
+
 [![Run on Repl.it](https://repl.it/badge/github/freeCodeCamp/boilerplate-express)](https://repl.it/github/freeCodeCamp/boilerplate-express)
--- myApp.js
+++ myApp.js
@@ -1,52 +1,58 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
 
-
 // --> 11)  Mount the body-parser middleware  here
 
-
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
 /** 3) Serve an HTML file */
-
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+/*
+app.get('/json', function(req, rse) {
+    res.json({
+        "message": "Hello json"
+    });
+});
+*/
 
 /** 6) Use the .env file to configure the app */
- 
- 
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-
 /** 8) Chaining middleware. A Time server */
 
-
 /** 9)  Get input from client - Route parameters */
-
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
-
 /** 12) Get data form POST  */
-
-
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
@@ -54,4 +60,4 @@ var app = express();
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
